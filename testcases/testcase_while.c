61a36cb08121b21293f9f2bf5d026d0465ce4a29
Dhanya Gowrish <66589227+dhanyagowrish@users.noreply.github.com>
2021-05-13 16:34:59 +0530

    add testcases

---
+++ testcases/testcase_while.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 20;
+	int b=3;
+	
+	while(a<50)
+	{
+		
+		printf("in while");
+		
+		a=a+1;
+		
+		int b=5;
+		
+		b=1+a;
+		
+		b=a+1;
+	
+		
+		int c = 5 + a;
+		
+		int d = a+7;
+		
+		int f = a;
+	
+	}
+		
+		
+	
+	
+	return 0;
+	
+	b=100;
+	
+}
+
+
+
+
+
